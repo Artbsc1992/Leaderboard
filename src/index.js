29fa17bc815182ea21e2a8c1777e4aae017e22cf
arturo <ahpareja@gmail.com>
2022-05-11 16:20:11 -0500

    Add API functionality

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import './style.css';
-import './modules/functions.js';
+import { setScores } from './modules/API.js';
+import { displayPlayers } from './modules/display.js';
+
+const submit = document.getElementById('submit');
+const refresh = document.querySelector('.refresh');
+const name = document.getElementById('name');
+const score = document.getElementById('score');
+
+displayPlayers();
+
+refresh.addEventListener('click', () => {
+    displayPlayers();
+});
+
+submit.addEventListener('click', () => {
+    setScores(name.value, score.value);
+    name.value= '';
+    score.value= '';
+});
---
+++ src/modules/display.js
@@ -0,0 +1,24 @@
+import { getScores } from "./API.js";
+
+const playerList = document.querySelector('.playerList');
+
+export const displayPlayers = () =>{
+    if(playerList.querySelectorAll('li')) {
+      Array.from(playerList.querySelectorAll('li')).forEach((li) =>{
+          playerList.removeChild(li);
+      });
+    }
+
+    getScores().then((result) => {
+        const orderScores = result.result.sort((a,b) => b.score - a.score);
+        orderScores.forEach((score) => {
+            const li = document.createElement('li');
+            li.className = 'item';
+            const p = document.createElement('p');
+            const text = document.createTextNode(`${score.user}: ${score.score}`);
+            p.appendChild(text);
+            li.appendChild(p);
+            playerList.appendChild(li);
+        });
+    });
+};
